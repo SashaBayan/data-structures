ec63480fe5db91fa6fd8c259b222383d495800f4
Sasha Bayan <SashaBayan@gmail.com>
2014-12-11 00:09:18 -0800

    ohhh mahhhh globbbb like totally completes queue and stack with shared junkkkkk gurllll

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,57 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  extend(someInstance, queueMethods);
+  this.counter = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+  	this[counter] = value;
+    counter++;
+  },
+  dequeue: function(){
+  	var result = this[0];
+  	delete this[0]
+
+
+    counter--;
+    if(counter < 0){
+      counter = 0;
+    }
+    for (var i = counter; i > 0; i--) {
+  	  console.log(this[i - 1]);
+  	  this[i - 1] = this[i];
+  	};
+    return result;
+  },
+  size: function(){
+  	return counter;
+  }
+};
 
 
 
+  each = function(collection, iterator) {
+    if(collection.constructor === Array){
+        for (var i = 0; i < collection.length; i++) {
+          iterator(collection[i], i, collection);
+        };
+    } else if(collection.constructor === Object){
+        var keys = Object.keys(collection);
+        for (var i = 0; i < keys.length; i++) {
+          iterator(collection[keys[i]], keys[i], collection);
+        };
+    }
+  };
+
+  extend = function(obj) {
+    each(arguments, function(item){
+      for(var key in item){
+        obj[key] = item[key]
+      }
+    });
+    return obj;
+  };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,61 @@
+
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  //Can we define counter in stackMethods?
+  this.counter = 0;
+
+  extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+	push: function(value){
+	  this[counter] = value;
+	  counter++;
+	},
+
+	pop: function(){
+	  var value = this[counter - 1];
+	  delete this[counter - 1];
+	  counter--;
+	  if(counter < 0){
+	  	counter = 0
+	  }
+	  return value;
+	},
+
+	size: function(){
+	  return counter;
+	}
+};
 
 
+
+  each = function(collection, iterator) {
+    if(collection.constructor === Array){
+        for (var i = 0; i < collection.length; i++) {
+          iterator(collection[i], i, collection);
+        };
+    } else if(collection.constructor === Object){
+        var keys = Object.keys(collection);
+        for (var i = 0; i < keys.length; i++) {
+          iterator(collection[keys[i]], keys[i], collection);
+        };
+    }
+  };
+
+  extend = function(obj) {
+    each(arguments, function(item){
+      for(var key in item){
+        obj[key] = item[key]
+      }
+    });
+    return obj;
+  };
+
+//.push, .pop, .size
